41c0f7c7a0eb41f9ef1591600c11e732d39f8b06
Matt <86365526+matthewstack@users.noreply.github.com>
2022-02-14 15:21:08 -0500

    sql commands

--- queries.sql
+++ queries.sql
@@ -8,11 +8,17 @@ SELECT Phone FROM [Suppliers] where SupplierID = 11
 
 -- List first 10 orders placed, sorted descending by the order date
 
+SELECT * FROM [Orders]  order by OrderDate desc limit 10
+
 -- Find all customers that live in London, Madrid, or Brazil
 
--- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+SELECT * FROM [Customers] where City = "London" or City = "Madrid" or Country = "Brazil"
 
+-- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+insert into Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+values("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth")
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+update Customers set PostalCode = "11122" where ContactName = "Bilbo Baggins"
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
